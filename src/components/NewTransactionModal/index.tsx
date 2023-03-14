8d6cb063bc5a29deea9ed13d088e932e2b1b149e
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-03-14 17:38:27 -0300

    feat: add basic structure for the transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import ReactModal from "react-modal";
+import { Container } from "./styles";
 
 ReactModal.setAppElement('#root');
 
@@ -12,8 +13,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input
+          type="text"
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </ReactModal>
   );
 }
